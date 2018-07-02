a9315b7062cf501cafae7bf820a32fd51d55d2e3
Carlo Carpio <carlo@omh.sg>
2018-07-02 14:44:14 +0800

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Carlo Carpio on 02/07/2018.
+//  Copyright © 2018 Carlo Carpio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
